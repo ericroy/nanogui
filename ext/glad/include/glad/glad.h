ebe962db41e9f0a878cefbd67bda8327cd76f6ce
Eric Roy <ericsroy@gmail.com>
2017-05-16 16:26:07 -0700

    Fix bug in glad

--- ext/glad/include/glad/glad.h
+++ ext/glad/include/glad/glad.h
@@ -50,7 +50,7 @@ typedef void* (* GLADloadproc)(const char *name);
 
 #ifndef GLAPI
 # if defined(GLAD_GLAPI_EXPORT)
-#  if defined(WIN32) || defined(__CYGWIN__)
+#  if defined(_WIN32) || defined(__CYGWIN__)
 #   if defined(GLAD_GLAPI_EXPORT_BUILD)
 #    if defined(__GNUC__)
 #     define GLAPI __attribute__ ((dllexport)) extern
